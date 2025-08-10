7fcc3859fa3f8fd755fe8fa0070f8a7b0c99e537
dipu-barman <dipub5522@gmail.com>
2025-08-10 16:46:53 +0600

    added-more-or-cors

--- index.js
+++ index.js
@@ -1,12 +1,9 @@
 
-
 const express = require('express');
 const cors = require('cors');
 require('dotenv').config();
-
 const jwt = require('jsonwebtoken');
-const cookieParser = require('cookie-parser'); // fixed typo
-
+const cookieParser = require('cookie-parser'); 
 const { MongoClient, ServerApiVersion, ObjectId } = require('mongodb');
 const app = express();
 const port = process.env.PORT || 3000;
@@ -14,64 +11,43 @@ const port = process.env.PORT || 3000;
 // middleware
 app.use(cookieParser());
 app.use(express.json());
-app.use(cors({
-  origin:['http://localhost:5173'],
-  credentials:true
-}))
+app.use(cors(
+//   {
+//   origin: [
+//     'http://localhost:5173',
+//      'https://my-assignment-11-server-rouge.vercel.app'
+//   ],
+//   credentials: true
+// }
+))
 
 // use logger......
 
-const logger=(req,res,next)=>{
+const logger = (req, res, next) => {
   console.log('inside the logger midel where')
   next()
 }
-
 // veryfie tooken ......
-
-const verifyToken = (req, res, next) =>{
-
-  const token = req?.cookies?.token;
-  console.log('cookie in the midelwere',req.cookies)
-   if (!token) {
+const verifyToken = (req, res, next) => {
+  const token = req.cookies?.token;
+  //console.log('cookie in the midelwere', req.cookies)
+  if (!token) {
     return res.status(401).send({ message: "unauthorized access" });
-   }
-
-// veryfie.....here
-
-   jwt.verify(token, process.env.JWT_ACCESS_SECRET, (err, decoded) => {
+  }
+  // veryfie.....here
+  jwt.verify(token, process.env.JWT_ACCESS_SECRET, (err, decoded) => {
     if (err) {
       return res.status(401).send({ message: "unauthorized access" });
-   }
-   console.log(decoded)
-   req.decoded = decoded;
+    }
+    console.log(decoded)
+    req.decoded = decoded;
     next();
   });
-
-    // 
-  // next()
 }
 
 
 
-// const allowedOrigins = [
-//   'https://assignment-11-41e71.web.app',
-//   'https://assignment-11-41e71.firebaseapp.com',
-//   'http://localhost:5173',
-//   'http://localhost:5173/pending'
-// ];
 
-
-// app.use(cors({
-//   origin: function(origin, callback) {
-//     if (!origin) return callback(null, true); // allow non-browser requests or same-origin
-//     if (allowedOrigins.indexOf(origin) !== -1) {
-//       callback(null, origin); // echo back the requesting origin if allowed
-//     } else {
-//       callback(new Error('Not allowed by CORS'));
-//     }
-//   },
-//   credentials: true
-// }));
 
 
 
@@ -104,67 +80,42 @@ const client = new MongoClient(uri, {
 
 async function run() {
   try {
-    await client.connect();
+    // await client.connect();
 
     const publicAssignmets = client.db('publicAssignmets').collection('assignmets');
     const allAssignmets = client.db('publicAssignmets').collection('assignmentmark');
     // const userCollection = client.db('publicAssignmets').collection('users'); // added
-    
-    
 
-    // jwt........
+
+
     
-    // app.post("/jwt", (req, res) => {
-    //   const user = req.body;
-    //   const token = jwt.sign(user, process.env.ACCESS_SECRET_TOKEN, {
-    //     expiresIn: "1d",
-    //   });
-    //   res.cookie("token", token, {
-    //     httpOnly: true,
-    //     secure: process.env.NODE_ENV === "production",
-    //     sameSite: process.env.NODE_ENV === "production" ? "none" : "lax" 
-    //   })
-    //     .send({ success: true, token });    
-    // });
 
 
     // jwt post ........
 
-    app.post("/jwt", async(req, res) => {
+    app.post("/jwt", async (req, res) => {
       const userData = req.body;
-      // const {email} = req.body;
-
-      const token = jwt.sign(userData, process.env.JWT_ACCESS_SECRET, 
-        {expiresIn: "1d",
-      });
-      res.cookie("token", token, {
-        httpOnly: true,
-        // secure: process.env.NODE_ENV === "production",
-         secure: false,
-        // sameSite: process.env.NODE_ENV === "production" ? "none" : "lax" 
-      })
-       res.send({ success: true });    
+      //const {email} = req.body;
+      const token = jwt.sign(userData, process.env.JWT_ACCESS_SECRET,
+        {
+          expiresIn: "1d"
+        });
+     
+        res.cookie("token", token, {
+          httpOnly: true,
+          secure: process.env.NODE_ENV === "production",
+          // secure: false
+          sameSite: process.env.NODE_ENV === "production" ? "none" : "strict" 
+        })
+        res.send({ success: true });
     });
-
-
-
-
-
-    // app.post("/logout", (req, res) => {
-    //   res
-    //     .clearCookie("token", {
-    //       httpOnly: true,
-    //       secure: process.env.NODE_ENV === "production",
-    //       sameSite: process.env.NODE_ENV === "production" ? "none" : "strict",
-    //     })
-    //     .send({ successfully_logOut: true });
-    // });
-
+    
     // all pubilc assignment .....
 
 
 
-    app.get('/assignmets', async (req, res) => {
+    app.get('/assignmets',async(req, res) => {
+      // const assignment =req.body
       // const email = req.query.email
       // if (email!==req.decoded.email){
       //   return res.status(403).send({message:'forbiden access'})
@@ -173,7 +124,7 @@ async function run() {
       // const query ={
       //   applicant:email
       // }
-      const result = await publicAssignmets.find(query).toArray();
+      const result = await publicAssignmets.find().toArray();
       res.send(result);
     });
 
@@ -183,14 +134,11 @@ async function run() {
       const result = await publicAssignmets.findOne(query);
       res.send(result);
     });
-
     app.post('/assignmets',  async (req, res) => {
       const newfree = req.body;
-      console.log('token is ', req.cookies);
       const result = await publicAssignmets.insertOne(newfree);
       res.send(result);
     });
-
     app.put('/assignmets/:id', async (req, res) => {
       try {
         const id = req.params.id;
@@ -220,15 +168,24 @@ async function run() {
 
     // get all assignment mark data
 
+    // app.get('/assignmentmark', async (req, res) => {
+      
+    //   const result = await allAssignmets.find().toArray();
+    //   res.send(result);
+    // });
+
+
     app.get('/assignmentmark', async (req, res) => {
+       console.log('inside...',req.cookies)
+      
       const result = await allAssignmets.find().toArray();
       res.send(result);
     });
 
 
-  //get specfic assignment mark
-  
-  
+    //get specfic assignment mark
+
+
     app.get('/assignmentmark/:id', async (req, res) => {
       const id = req.params.id;
       const query = { _id: new ObjectId(id) };
@@ -240,7 +197,7 @@ async function run() {
 
     app.post('/assignmentmark', async (req, res) => {
       const newfree = req.body;
-      console.log('token  is ', req.cookies)
+      // nod
       const result = await allAssignmets.insertOne(newfree);
       res.send(result);
     });
